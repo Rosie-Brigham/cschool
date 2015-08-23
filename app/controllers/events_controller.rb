0dabe8194598a7b71566d03b6364b76351f8cb9f
Rosie-Brigham <rosie@woolandthegang.com>
2015-08-23 11:22:20 +0100

    initial comit

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,15 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  # GET /events
+  # GET /events.json
+  def index
+    @events = Event.all
+  end
+
+  # GET /events/1
+  # GET /events/1.json
+  def show
+  end
+
+end
