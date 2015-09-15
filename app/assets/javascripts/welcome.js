0664e8464a28e13292fd8aa50820277179f75540
Rosie-Brigham <rosie@woolandthegang.com>
2015-09-15 22:24:07 +0100

    tidies welcome js

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,17 +1,10 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+
 
 $(document).ready(function() {
- 
   $("#owl-demo").owlCarousel({
- 
       autoPlay: 3000, //Set AutoPlay to 3 seconds
- 
       items : 4,
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3]
- 
   });
- 
 });
